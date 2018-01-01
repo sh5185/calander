cb93e204c89cbbd839b2b6e8adcb7e6a0b175f24
승호 <@DESKTOP-1K53R8K>
2018-01-01 23:50:50 +0900

    clear question

--- src/calender/Calender.java
+++ src/calender/Calender.java
@@ -1,14 +1,34 @@
 package calender;
 
-public class Calender {
+import java.util.Scanner;
 
+public class Calender {
+	private static final int[] MAX_DAYS= {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
 	public static void main(String[] args) {
-		System.out.println("일 월 화 수 목 금 토");
-		System.out.println("----------------------");
-		System.out.println("  1  2   3  4   5   6  7");
-		System.out.println("  8  9  10 11 12 13 14");
-		System.out.println(" 15 16 17 18 19 20 21");
-		System.out.println(" 22 23 24 25 26 27 28");
+		Scanner scan = new Scanner(System.in );
+		Calender c = new Calender();
+		int month;
+		System.out.println("");
+		
+				System.out.println("몇월 달력을 출력하시겠습니까?");
+				month=scan.nextInt();
+				while(month>0) {
+				int date=c.getMaxDaysOfMonth(month);
+				System.out.println("일     월     화     수     목     금     토");		
+					for(int day=1 ;day<date;) {
+						for( int count=0; count<7 ;count++,day++) {
+							if(day>date) {
+								break;
+							}
+							System.out.print(day+"\t");
+						}
+						System.out.println();
+					}
+				}	
 	}
 
 }
